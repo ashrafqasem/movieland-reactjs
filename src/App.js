97491a3a9b2a62f7d91e382d50aa150004b9a17e
Ashraf Qasem <ashraf_qasem@hotmail.com>
2024-08-07 18:22:30 -0400

    dddd__rrr

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import MovieCard from './MovieCard'
 
 import axios from 'axios';
 
-const movie1 = {
-  "Title": "Italian Spiderman",
-  "Year": "2007",
-  "imdbID": "tt2705436",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
-};
+// const movie1 = {
+//   "Title": "Italian Spiderman",
+//   "Year": "2007",
+//   "imdbID": "tt2705436",
+//   "Type": "movie",
+//   "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
+// };
 
 function App() {
 
